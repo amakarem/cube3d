4a00d0f3b7e084f6f600e5d7babff5c9e556275b
Tobias Keil <tobiaskeil.dev@gmail.com>
2025-04-04 00:17:45 +0200

    Update raycast.c

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -47,26 +47,20 @@ int	ft_raycast(t_data **data)
     float   fov_rad_2;
     float   section[(*data)->wnd_w];
 
-	// printf("in the raycastin fun. (*data)->wnd_w = %i\n", (*data)->wnd_w);
     ft_clean_window(*data);
     ft_update_players_pos_and_dir(data, (*data)->player.angle);
     x = 0;
-    // fov_rad_2 = (FOV * (180 / M_PI)) / 2;
-	fov_rad_2 = ((float)FOV / 2) * (M_PI / 180);
+    fov_rad_2 = ((float)FOV / 2) * (M_PI / 180);
     incr = ((float)FOV / ((*data)->wnd_w - 1)) * (M_PI / 180);
-	// printf("before incr loop (*data)->player.angle = %f, incr = %f\n", (*data)->player.angle * (180 / M_PI), incr);
     while (x < (*data)->wnd_w)
     {
         section[x] = (fov_rad_2 - incr * x) + (*data)->player.angle;
-		// printf("section[%i] = %f rad and %f degrees\n", x, section[x], section[x] * 180 / M_PI);
         x++;
     }
     x = 0;
     while (x < (*data)->wnd_w)
     {
-		printf("before draw wall\n");
         ft_draw_wall_slice(*data, &(*data)->buffer, section, x);
-		printf("after draw wall\n\n");
         x++;
     }
 	ft_putpxl(&(*data)->buffer, 5, 5, 0xff0000);
