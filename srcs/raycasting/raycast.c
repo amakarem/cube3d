24e9c55308d9bb23154170203044bec87d57ee19
Tobias Keil <tobiaskeil.dev@gmail.com>
2025-04-03 13:01:08 +0200

    simplified the calculation of section increments

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -6,7 +6,7 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 16:51:40 by tkeil             #+#    #+#             */
-/*   Updated: 2025/04/03 05:02:30 by tkeil            ###   ########.fr       */
+/*   Updated: 2025/04/03 13:00:48 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@ int	ft_raycast(t_data **data)
 {
     int     x;
     float   incr;
+    float   fov_rad_2;
     float   section[(*data)->wnd_w];
     
     ft_clean_window(*data);
     x = 0;
+    fov_rad_2 = FOV * (180 / M_PI) / 2;
     incr = (FOV / ((*data)->wnd_w - 1)) * (180 / M_PI);
     while (x < (*data)->wnd_w)
     {
-        section[x] = incr * x - (*data)->player.angle;
+        section[x] = (fov_rad_2 - incr * x) - (*data)->player.angle;
         x++;
     }
     x = 0;
