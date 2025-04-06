c0bb54b9532c0b2ab32a81c1310507e8a63c6517
Tobias Keil <tobiaskeil.dev@gmail.com>
2025-04-06 15:48:18 +0200

    improved the ft_raycast description.

--- srcs/raycasting/raycast.c
+++ srcs/raycasting/raycast.c
@@ -6,22 +6,26 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/30 16:51:40 by tkeil             #+#    #+#             */
-/*   Updated: 2025/04/06 14:50:05 by tkeil            ###   ########.fr       */
+/*   Updated: 2025/04/06 15:47:44 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D.h"
 
-// Raycasting Tutorial on: https://lodev.org/cgtutor/raycasting.html
+// Raycasting Tutorial: https://lodev.org/cgtutor/raycasting.html
 //
-// one ray for each x coordinate on the screen
-// => representing the Field of View (FOV) of the player
+// Main raycasting loop. One vertical ray is cast for every x-coordinate on the screen,
+// covering the player's entire Field of View (FOV).
 //
-// cameraX is the x coordinate on the camera plane of the current x -
-// coordinate on the screen and goes from -1 to +1 (left to right) (normalized).
+// For each screen column:
+// - cameraX is calculated (normalized range: -1.0 to +1.0, left to right)
+// - rayDir is computed using the player's direction and the camera plane:
+//   rayDir = playerDir + plane * cameraX
 //
-// rayDir (vector) = players_dir + plane * cameraX
-// camera plane is always perpendicular on the players direction.
+// The camera plane is always perpendicular to the viewing direction and defines the FOV.
+//
+// Each ray is used to render one vertical slice of the scene (ceiling, wall, floor).
+// Slices are rendered to the image buffer and put to the screen afterwards.
 int	ft_raycast(t_data *data)
 {
     int         x;
